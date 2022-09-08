9b23e2696171f5b8a7ff32da8e92bd63f2ca1a3e
Your Namegit config --global user.name Gustavo <gustavosypr@gmail.com>
2022-09-08 12:17:59 -0700

    Framework of Branch.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -12,7 +12,6 @@ let marker = L.circle([34.0522, -118.2437], {
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-// We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {    
     maxZoom: 18,
     accessToken: API_KEY
